8def68b753a359848459aba13282f98883dd45de
rafael <rafadoria1@gmail.com>
2022-02-04 23:38:32 +0900

    Added changing color on mouse down functionality

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const container = document.querySelector('.container')
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
 
 function createGrid(size) {
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`
@@ -7,8 +10,15 @@ function createGrid(size) {
     for (i=0; i<size * size; i++) {
         const cell = document.createElement('div');
         cell.classList.add('cell');
+        cell.addEventListener('mouseover', changeColor);
+        cell.addEventListener('mousedown', changeColor);
         container.appendChild(cell);
     }
+}
+
+function changeColor(e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+    else e.target.style.backgroundColor = '#3b3b3b'
 }
 
 createGrid(32)
